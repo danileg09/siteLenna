308760c448641044dbc0265a0e9eb7360fd01b67
daniel <danileg09@gmail.com>
2023-09-16 22:26:57 -0300

    sla

---
+++ js/teste002.js
@@ -0,0 +1,9 @@
+var texto = "Centro de Valorização da Vida ligue 188";
+
+function modfTexto1(){
+    window.location.href = "https://www.psicologiaviva.com.br"
+}
+
+function modfTexto2(){
+    document.getElementById("teste002.1").innerHTML = texto;
+}
